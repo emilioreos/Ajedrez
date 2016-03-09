adb236214b40724cf21037df44062cc9b38abd8f
Emilio <Emilio@EmilioReos-PC>
2016-03-08 18:00:48 -0600

    getPieza de Tablero

--- ajedrez/src/ajedrez/Tablero.java
+++ ajedrez/src/ajedrez/Tablero.java
@@ -3,8 +3,31 @@ package ajedrez;
 public class Tablero {
 	public Pieza[] blancas;
 	public Pieza[] negras;
-	public boolean getPosicion(char columna,byte fila){
+	public boolean getPosicion(char columna,byte fila,Color color){
+		if(columna<'a'||fila<1){
+			return false;
+		}else{
+			for(int i=0;i<blancas.length;i++){
+				if(blancas[i]!=null){
+					
+				}
+			}
+		}
 		return false;
 	}
-	
+	public Pieza getPieza(char Columna,byte fila){
+		for(int i=0;i<blancas.length;i++){
+			if(blancas[i]!=null){
+				if(blancas[i].fila==fila&&blancas[i].columna==Columna){
+					return blancas[i];
+				}
+			}
+			if(negras[i]!=null){
+				if(negras[i].fila==fila&&negras[i].columna==Columna){
+					return negras[i];
+				}
+			}
+		}
+		return null;
+	}
 }
